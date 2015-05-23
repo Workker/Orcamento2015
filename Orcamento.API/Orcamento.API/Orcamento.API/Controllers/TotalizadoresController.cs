f4ec2762018384ba799b49a60982674d49170dbd
Sammuel Garcia <garciasammuel@gmail.com>
2015-05-23 19:02:40 -0300

    Commit final

---
+++ Orcamento.API/Orcamento.API/Orcamento.API/Controllers/TotalizadoresController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Orcamento.API.Controllers
+{
+
+    public class Totalizador
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+    }
+
+
+    public class TotalizadoresController : ApiController
+    {
+        //
+        // GET: /Totalizadores/
+
+        private static List<Totalizador> totalizadores;
+
+        [System.Web.Http.HttpGet]
+        public List<Totalizador> GET()
+        {
+            return totalizadores;
+        }
+
+        [System.Web.Http.HttpPost]
+        public List<Totalizador> POST(string nome, string valor)
+        {
+            if (totalizadores == null)
+                totalizadores = new List<Totalizador>();
+
+            totalizadores.Add(new Totalizador() { Id = Guid.NewGuid(), Nome = nome, Valor = valor });
+
+            return totalizadores;
+        }
+    }
+}
